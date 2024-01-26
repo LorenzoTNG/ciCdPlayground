746966a4a1dbe8d4c777261f8e495141ff4cd67e
jacquerl <lorenzo.jacqueroud@tngtech.com>
2024-01-26 10:14:05 +0100

    Change name

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ new App({
 		
 		// What do you associate with the term 'CI/CD'?
 		associations: [
-			'Ehm, what?'
+			'Ehm, what? '
 		],
 		// Which CI/CD tools do you use in your project?
 		tools: 'Jenkins, maybe?',
